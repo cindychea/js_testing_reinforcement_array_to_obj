84ed183ccf0d078483448eccec4b9c4294e44232
cindychea <cindy_chea@hotmail.com>
2019-07-02 11:21:08 -0400

    completed testing reinforcement exercise: converts array to object

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function arrayToObj() {
-   // your code goes here  
+function arrayToObj(A) {
+   const obj = {};
+   A.forEach(([k,v]) => {
+      obj[k] = v;
+   })
+   return obj
 }
-
 module.exports = arrayToObj;
+
